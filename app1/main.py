ea0b8364cfa3a119f4e5ba95eec0f4a4a7cc2fb8
ming <archaicdust@gmail.com>
2016-08-02 22:32:45 +0800

    logging to stdout

--- app1/main.py
+++ app1/main.py
@@ -1,9 +1,11 @@
 import logging
-
+import sys
 
 logger = logging.getLogger(__name__)
 
-logger.addHandler(logging.StreamHandler())
+#logger.addHandler(logging.StreamHandler())
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
 logger.setLevel(logging.INFO)
 
 logger.info('hello world')
